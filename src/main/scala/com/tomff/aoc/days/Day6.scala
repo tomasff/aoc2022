40d71c23f01c4a739d250af8c7b92bab8836d64f
tomasff <15079464+tomasff@users.noreply.github.com>
2022-12-06 18:51:25 +0000

    Refactor day 6

--- src/main/scala/com/tomff/aoc/days/Day6.scala
+++ src/main/scala/com/tomff/aoc/days/Day6.scala
@@ -3,12 +3,12 @@ package com.tomff.aoc.days
 import com.tomff.aoc.Day
 
 object Day6 extends Day(6) {
-  private def findPacketMarkerStart(input: String, markerLength: Int): Int =
+  private def findPacketMarkerStart(input: Seq[Char], markerLength: Int): Int =
     input.sliding(markerLength).zipWithIndex.find(
       (packet, _) => packet.distinct.length == packet.length
-    ).map(_._2).get + markerLength
+    ).map(_._2 + markerLength).getOrElse(-1)
 
   override def solve(input: Seq[String]): (Int, Int) = {
-    (findPacketMarkerStart(input.head, 4), findPacketMarkerStart(input.head, 14))
+    (findPacketMarkerStart(input.flatten, 4), findPacketMarkerStart(input.flatten, 14))
   }
 }
